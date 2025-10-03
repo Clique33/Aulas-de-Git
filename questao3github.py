82ea807eac106f3523d752c1d43b69c13302bdde
joaopps1974 <joao.pereira.ismart@gmail.com>
2025-10-03 12:35:19 -0300

    Consertando questao3github.py

--- questao3github.py
+++ questao3github.py
@@ -11,22 +11,4 @@ def pangrama(frase: str) -> bool:
 print(pangrama("a quick brown fox jumps over the lazy dog"))  
 print(pangrama("the five boxing wizards jump quickly"))  
 
-#============================
-import random
 
-
-def random_case(s: str) -> str:
-    resultado = ""
-    for letra in s:
-        if random.randint(0, 1) == 0:
-            resultado += letra.lower()
-        else:
-            resultado += letra.upper()
-    return resultado
-
-
-print(random_case("python"))
-print(random_case("Algoritmo"))
-print(random_case("Paralelepipedo"))
-
-    
